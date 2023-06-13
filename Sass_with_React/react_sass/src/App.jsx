7ff7b9ef294fc96ba0669da89890884e24220285
unknown <arthurguilherme2013@gmail.com>
2023-06-13 07:46:20 +0100

    Project Shop Electrum and Curriculum with React

---
+++ Sass_with_React/react_sass/src/App.jsx
@@ -0,0 +1,14 @@
+import './sass/components/app.sass'
+
+function App() {
+
+  return (
+    <>
+      <div id="container">
+        <h1>React + SASS</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
